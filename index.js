17385b0381a9420611132634c2ebfe2671163550
Raryson Pereira Rost <raryson.rost@gmail.com>
2019-02-05 14:58:49 -0200

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app =  express()
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.send('index.html'))
+
+app.listen(process.env.PORT || 3000)
